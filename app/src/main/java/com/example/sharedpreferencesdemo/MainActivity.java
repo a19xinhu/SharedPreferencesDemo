5f4348d7892b7691e54b2abe8e8e17cac8e428c6
nsa <tmxk93@hotmail.com>
2022-05-03 10:53:49 +0200

    Add Shared pref for label text

--- app/src/main/java/com/example/sharedpreferencesdemo/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencesdemo/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.sharedpreferencesdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button storePrefButton;
+    private TextView labelTextView;
+    private EditText inputEditView;
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        String pref = myPreferenceRef.getString("GregerRef","Saknar data");
+
+        labelTextView = findViewById(R.id.textLabel);
+        labelTextView.setText(pref);
+        inputEditView = findViewById(R.id.textInput);
+
+        storePrefButton = findViewById(R.id.storePref);
+        storePrefButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                labelTextView.setText(inputEditView.getText());
+            }
+        });
     }
 }
